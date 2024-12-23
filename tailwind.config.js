2ff0c554f9ff215bf51306496b04a212010e6db3
Uzumaki-Shuvro <147752343+Uzumaki-Shuvro@users.noreply.github.com>
2024-12-23 23:14:52 +0600

    aksadoi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+// filepath: /d:/Game Development/Flappy Bird/tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths according to your project structure
+  ],
   theme: {
     extend: {},
   },
