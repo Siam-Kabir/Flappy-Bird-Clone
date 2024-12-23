1630081f3da5851ad184e6250ca0557c39fccc57
Uzumaki-Shuvro <147752343+Uzumaki-Shuvro@users.noreply.github.com>
2024-12-24 00:35:52 +0600

    aksadoi

--- src/hooks/useCollisionDetection.ts
+++ src/hooks/useCollisionDetection.ts
@@ -9,7 +9,7 @@ export const useCollisionDetection = (config: CollisionConfig) => {
     const birdHitbox = getBirdHitbox(bird);
 
     // Check floor collision
-    if (birdHitbox.y + birdHitbox.height >= window.innerHeight - GAME_CONFIG.CANVAS.GROUND_HEIGHT)  {
+    if (birdHitbox.y + birdHitbox.height >= window.innerHeight -200) {
       return true;
     }
 
